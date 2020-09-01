702c8057b07675162335b090450df1cf32a35029
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-01 12:09:23 -0600

    formatting

--- front-end/src/pages/Dashboard.js
+++ front-end/src/pages/Dashboard.js
@@ -4,7 +4,7 @@ export default function Dashboard() {
 
     return (
         <div>
-            Dashboard
+            Dashboardad
         </div>
     )
 }
--- front-end/src/pages/Error.js
+++ front-end/src/pages/Error.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Error() {
-
-    return (
-        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '200px'}}>
-            <img src="https://upload.wikimedia.org/wikipedia/en/7/79/The_Simpsons-Jeff_Albertson.png"/>
-            <h2> Worst Error Page Ever!!!</h2>
-            <Link to='/dashboard'>Return To Dashboard</Link>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "200px",
+      }}
+    >
+      <img src="https://upload.wikimedia.org/wikipedia/en/7/79/The_Simpsons-Jeff_Albertson.png" />
+      <h2> Worst Error Page Ever!!!</h2>
+      <Link to="/dashboard">Return To Dashboard</Link>
+    </div>
+  );
 }
