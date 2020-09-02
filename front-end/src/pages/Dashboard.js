20d5da84dd152fc1a270b05b4c86ae906c2ba0f2
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-02 16:45:53 -0600

    Dashboard Title

--- front-end/src/pages/Dashboard.js
+++ front-end/src/pages/Dashboard.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Dashboard(props) {
-  return <div></div>;
+  return ( 
+    <div style={{textAlign: 'center'}}>
+      <h1>Welcome to the Dashboard</h1>
+    </div>
+
+  )
 }
