9dc7241816950053046070b942a59182d0c66122
Philip Ng <philipn@corebridge.net>
2020-09-02 17:05:54 -0600

    refactored error page

--- front-end/src/pages/Error.js
+++ front-end/src/pages/Error.js
@@ -1,21 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Error.scss";
+import comicBookGuy from "../images/comic-book-guy.png";
+
 export default function Error() {
   return (
-    <div
-      style={{
-        fontFamily: "SimpsonsFont",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        marginTop: "200px",
-      }}
-    >
-      <img
-        src="https://upload.wikimedia.org/wikipedia/en/7/79/The_Simpsons-Jeff_Albertson.png"
-        alt="Comic Book Guy"
-      />
+    <div className="error-wrapper">
+      <img src={comicBookGuy} alt="Comic Book Guy" />
       <h2> Worst Error Page Ever!!!</h2>
       <Link to="/dashboard">Return To Dashboard</Link>
     </div>
