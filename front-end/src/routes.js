e4d99e060aacb5aa97cc9c884a76d3d89edefdab
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-04 12:16:03 -0600

    Error Page Formattion

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -1,20 +1,19 @@
-import React from "react"
-import {Switch, Route} from 'react-router-dom'
-import Auth from './pages/Auth/Auth'
-import Dashboard from './pages/Dashboard'
-import Episodes from './pages/Episodes'
-import Profile from './pages/Profile'
-import SingleEpisode from './pages/SingleEpisode'
-import Error from './pages/Error'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Auth from "./pages/Auth/Auth";
+import Dashboard from "./pages/Dashboard";
+import Episodes from "./pages/Episodes";
+import Profile from "./pages/Profile";
+import SingleEpisode from "./pages/SingleEpisode";
+import Error from "./pages/Error";
 
 export default (
-  
-    <Switch>
-        <Route exact path='/' component={Auth}/>
-        <Route path='/dashboard' component={Dashboard}/>
-        <Route path='/profile/:user_id' component={Profile}/>
-        <Route exact path='/episodes' component={Episodes}/>
-        <Route path='/episodes/:episode' component={SingleEpisode}/>
-        <Route path='*' component={Error}/>
-    </Switch>
-)   
+  <Switch>
+    <Route exact path="/" component={Auth} />
+    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/profile/:user_id" component={Profile} />
+    <Route exact path="/episodes" component={Episodes} />
+    <Route path="/episodes/:episode" component={SingleEpisode} />
+    <Route path="*" component={Error} />
+  </Switch>
+);
