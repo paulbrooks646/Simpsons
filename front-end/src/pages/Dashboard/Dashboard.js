c854943292ee90b4c3b5e66215fc600b33221296
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-07 13:36:08 -0600

    background height and width

--- front-end/src/pages/Dashboard/Dashboard.js
+++ front-end/src/pages/Dashboard/Dashboard.js
@@ -8,9 +8,7 @@ export default function Dashboard(props) {
   return (
     <Page>
       <div className="dashboard-main" style={{backgroundImage: `url(${Background})`}}>
-        <div className="dashboard-title-div">
           <h1 className="dashboard-title">Welcome to The Android's Dungeon</h1>
-        </div>
       </div>
     </Page>
   );
