1f79bd1a6b2faa0bed9d58a2680ec004d3722582
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-07 13:18:41 -0600

    auth background fix

--- front-end/src/pages/Dashboard/Dashboard.js
+++ front-end/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Page from "../../components/Page";
 import "./Dashboard.scss";
+import Background from "../../images/androids-dungeon-bg.png"
+
 
 export default function Dashboard(props) {
   return (
     <Page>
-      <div className="dashboard-main">
+      <div className="dashboard-main" style={{backgroundImage: `url(${Background})`}}>
         <div className="dashboard-title-div">
           <h1 className="dashboard-title">Welcome to The Android's Dungeon</h1>
         </div>
