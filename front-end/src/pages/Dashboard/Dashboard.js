e4294395a81ca762d5079602ae2de30cf3e6ba47
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-07 11:44:05 -0600

    dashboard background image

---
+++ front-end/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Page from "../../components/Page";
+import "./Dashboard.scss";
+
+export default function Dashboard(props) {
+  return (
+    <Page>
+      <div className="dashboard-main">
+        <h1 className="dashboard-title">Welcome to The Android's Dungeon</h1>
+      </div>
+    </Page>
+  );
+}
