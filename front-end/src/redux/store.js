ed16c8a2ff6a0796c6de64fadb825fa139c6ef39
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-08 14:13:05 -0600

    added episode reducer to store

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import userReducer from "./userReducer";
 import promiseMiddleware from "redux-promise-middleware";
+import episodesReducer from "./episodesReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  episodes: episodesReducer
 });
 
 export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
