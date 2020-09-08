dc411d0300d9d3959717a8bc1ab34c3b7c7cbb9d
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-08 14:19:14 -0600

    episodes component

--- front-end/src/pages/Episodes/Episodes.js
+++ front-end/src/pages/Episodes/Episodes.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Page from "../../components/Page";
 import './Episodes.scss'
+import axios from "axios";
+import { getEpisodes } from '../../redux/episodesReducer'
+import {connect} from 'react-redux'
 
-export default function Episodes() {
+function Episodes(props) {
+
+  useEffect(() => {
+    axios.get("/episodes").then(() => {
+      props.getEpisodes(res.data)
+    })
+  }, [props.getEpisodes])
+
+
   return (
     <Page>
       <div>Episodes</div>
     </Page>
   );
 }
+
+const mapStateToProps = (reduxState) => reduxState;
+export default connect(mapStateToProps, { getAttractions })(Episodes);
