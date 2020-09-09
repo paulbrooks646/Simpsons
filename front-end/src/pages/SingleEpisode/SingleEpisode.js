c38744ca41aa65d3f0dd8fd9afeba57f16a8f05d
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-09 13:08:25 -0600

    episode axios call

--- front-end/src/pages/SingleEpisode/SingleEpisode.js
+++ front-end/src/pages/SingleEpisode/SingleEpisode.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Page from "../../components/Page";
 import "./SingleEpisode.scss";
+import axios from "axios";
 
-export default function SingleEpisode() {
+export default function SingleEpisode(props) {
+
+  const [info, setInfo] = useState()
+
+  useEffect(() => {
+    const episode = props.match.params.episode.replace(/_/g, " ")
+    axios.get(`/episode/${episode}`)
+      .then(res => {
+      setInfo(res.data)
+    })
+    
+}, [props.match.params.episode])
+
+
+
+
   return (
     <Page>
       <div>
-        
+
       </div>
     </Page>
   );
