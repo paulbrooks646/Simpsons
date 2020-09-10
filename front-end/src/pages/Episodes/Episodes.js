8487bf1454a90c058162dabef3d79fffc98bc975
Philip Ng <philipn@corebridge.net>
2020-09-10 11:32:07 -0600

    added basic pagination on Episodes page

--- front-end/src/pages/Episodes/Episodes.js
+++ front-end/src/pages/Episodes/Episodes.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Page from "../../components/Page";
 import "./Episodes.scss";
 import axios from "axios";
 import { getEpisodes } from "../../redux/episodesReducer";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import Pagination from "@material-ui/lab/Pagination";
 
 function Episodes(props) {
   const { getEpisodes } = props;
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     axios.get("/episodes").then((res) => {
@@ -15,21 +18,36 @@ function Episodes(props) {
     });
   }, [getEpisodes]);
 
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
   const episodes = props.episodes.info.map((e, index) => {
-    return (
-      <Link key={index} to={`/episodes/${e.episode_name.replace(/ /g, "_")}`}>
-        <div className="episodes-episode">
-          <h1>{e.episode_name}</h1>
-          <img src={e.episode_image} alt="simpsons" />
-          <h6>{e.episode_synopsis}</h6>
-        </div>
-      </Link>
-    );
+    if (e.season === page) {
+      return (
+        <Link key={index} to={`/episodes/${e.episode_name.replace(/ /g, "_")}`}>
+          <div className="episodes-episode">
+            <h1>{e.episode_name}</h1>
+            <img src={e.episode_image} alt="simpsons" />
+            <h6>{e.episode_synopsis}</h6>
+          </div>
+        </Link>
+      );
+    }
   });
 
   return (
     <Page>
       <div className="episode-main">{episodes}</div>
+      <Typography style={{ textAlign: "center" }}>Season: {page}</Typography>
+      <Pagination
+        count={10}
+        color="primary"
+        page={page}
+        onChange={handleChange}
+        showFirstButton
+        showLastButton
+      />
     </Page>
   );
 }
