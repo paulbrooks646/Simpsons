ba799529cee73227e764e83a86da65c80169441b
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-11 11:57:15 -0600

    combining ratings

--- back-end/controllers/episodesController.js
+++ back-end/controllers/episodesController.js
@@ -44,35 +44,35 @@ module.exports = {
     const db = req.app.get("db")
 
     db.get_ratings().then(ratings => {
-      // // console.log(ratings)
-      // let newArr = []
+      console.log(ratings)
+      let newArr = []
       
-      // for (let i = 1; i < ratings.length; i++) {
-      //   let total = 0;
-      //   let avg = 0;
-      //   let combined = {
-      //     episode_name: ratings[i].episode_name,
-      //   };
+      for (let i = 1; i < ratings.length; i++) {
+        let total = 0;
+        let avg = 0;
+        let combined = {
+          episode_name: ratings[i].episode_name,
+        };
 
-      //   ratings.forEach((element) => {
-      //     if (ratings[i].episode_name === element.episode_name) {
-      //       avg += 1;
-      //       total += element.rating;
-      //     }
-      //   });
-      //   combined.rating = (total / avg).toFixed(1);
-      //   newArr.push(combined)
-      // }
+        ratings.forEach((element) => {
+          if (ratings[i].episode_name === element.episode_name) {
+            avg += 1;
+            total += +element.rating;
+          }
+        });
+        combined.rating = (total / avg).toFixed(1);
+        newArr.push(combined)
+      }
 
-      // for (let i = 0; i < newArr.length; i++) {
-      //   for (let j = newArr.length - 1; j > i; j--) {
-      //     if (newArr[i].episode_name === newArr[j].episode_name) {
-      //       newArr.splice(j , 1)
-      //     }
-      //   }
-      // }
-      // newArr.sort((a, b) => a.rating.length > b.rating.length ? -1 : a.rating < b.rating ? 1 : -1);
-      res.status(200).send(ratings)
+      for (let i = 0; i < newArr.length; i++) {
+        for (let j = newArr.length - 1; j > i; j--) {
+          if (newArr[i].episode_name === newArr[j].episode_name) {
+            newArr.splice(j , 1)
+          }
+        }
+      }
+      newArr.sort((a, b) => a.rating.length > b.rating.length ? -1 : a.rating < b.rating ? 1 : -1);
+      res.status(200).send(newArr)
     })
   }
 };
