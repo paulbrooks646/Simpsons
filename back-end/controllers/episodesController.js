a9acec80afc5446715e4ead493bfc6e05d17b843
Philip Ng <philipn@corebridge.net>
2020-09-15 15:56:47 -0600

    showing reviews for single episode

--- back-end/controllers/episodesController.js
+++ back-end/controllers/episodesController.js
@@ -25,7 +25,12 @@ const getEpisode = (req, res) => {
         if (episodes[i].episode_name === element.episode_name) {
           avg += 1;
           total += +element.rating;
-          combined.reviews.push([element.review, element.username, element.profile_pic]);
+          combined.reviews.push([
+            element.rating,
+            element.review,
+            element.username,
+            element.profile_pic,
+          ]);
         }
       });
       combined.rating = (total / avg).toFixed(1);
@@ -93,7 +98,11 @@ const getEpisodes = (req, res) => {
         if (episodes[i].episode_name === element.episode_name) {
           avg += 1;
           total += +element.rating;
-          combined.reviews.push([element.review, element.username, element.profile_pic]);
+          combined.reviews.push([
+            element.review,
+            element.username,
+            element.profile_pic,
+          ]);
         }
       });
       combined.rating = (total / avg).toFixed(1);
