b9a067901cbb4edf88f11d9d80ee4a75304038cb
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-17 13:53:01 -0600

    character list card

--- front-end/src/pages/Characters/Characters.js
+++ front-end/src/pages/Characters/Characters.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import "./Characters.scss";
 import Page from "../../components/Page";
 import axios from "axios";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
 
 export default function Characters(props) {
-  const [characters, setCharacters] = useState();
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     axios.get("/characters").then((res) => {
@@ -12,9 +14,33 @@ export default function Characters(props) {
     });
   }, []);
 
+  const characterList = characters.map((e, index) => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+                alignItems: "center",
+                marginTop: "50px",
+          flexDirection: 'column'
+        }}
+      >
+        <Card variant="outlined" className="character-card">
+          <img src={e.picture} style={{ width: "80%", height: "90%" }} />
+          <h6 style={{ zIndex: "5", position: "absolute", top: "110px"}}>
+            {e.name}
+          </h6>
+            </Card>
+            <Card className="character-detail-card" style={{width: '500px', height: "500px"}}>
+
+            </Card>
+      </div>
+    );
+  });
+
   return (
     <Page>
-      <div>Characters</div>
+      <div>{characterList}</div>
     </Page>
   );
 }
