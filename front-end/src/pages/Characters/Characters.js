7c4bff83c07db81a7730a33e9e03f10659167192
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-17 13:10:30 -0600

    get characters axios call

--- front-end/src/pages/Characters/Characters.js
+++ front-end/src/pages/Characters/Characters.js
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Characters.scss";
 import Page from "../../components/Page";
+import axios from "axios";
 
 export default function Characters(props) {
+  const [characters, setCharacters] = useState();
+
+  useEffect(() => {
+    axios.get("/characters").then((res) => {
+      setCharacters(res.data);
+    });
+  }, []);
+
   return (
     <Page>
       <div>Characters</div>
