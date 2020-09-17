637adfe2c1fda2f2f1ff0e76dd539a624811c501
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-17 14:56:11 -0600

    Single Character skeleton structure

---
+++ front-end/src/pages/SingleCharacter/SingleCharacter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SingleCharacter.scss'
+
+export default function SingleCharacter() {
+
+    return (
+        <div>
+            SingleCharacter
+        </div>
+    )
+}
