38e7c9a2007795c6296c0160d380de789708544c
Philip Ng <philipn@corebridge.net>
2020-09-17 15:32:38 -0600

    added forgot password UI; added Roboto font CDN

--- front-end/src/pages/Auth/Auth.js
+++ front-end/src/pages/Auth/Auth.js
@@ -15,6 +15,14 @@ import FormHelperText from "@material-ui/core/FormHelperText";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import Link from "@material-ui/core/Link";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+import Snackbar from "@material-ui/core/Snackbar";
+import Alert from "@material-ui/lab/Alert";
 
 import { loginUser, registerUser } from "../../redux/userReducer";
 import Clouds from "../../images/clouds.jpg";
@@ -29,7 +37,42 @@ function Auth(props) {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordsMatch, setPasswordsMatch] = useState(true);
   const [account, setAccount] = useState(true);
+  const [forgotPasswordDialogIsOpen, setForgotPasswordDialogIsOpen] = useState(
+    false
+  );
+  const [forgotPasswordEmail, setForgotPasswordEmail] = useState("");
+  const [infoSnackbarIsOpen, setInfoSnackbarIsOpen] = useState(false);
+  const [errorSnackbarIsOpen, setErrorSnackbarIsOpen] = useState(false);
 
+  function handleInfoSnackbarOpen() {
+    setInfoSnackbarIsOpen(true);
+  }
+
+  function handleInfoSnackbarClose() {
+    setInfoSnackbarIsOpen(false);
+  }
+
+  function handleErrorSnackbarOpen() {
+    setErrorSnackbarIsOpen(true);
+  }
+
+  function handleErrorSnackbarClose() {
+    setErrorSnackbarIsOpen(false);
+  }
+
+  function handleForgotPasswordDialogOpen() {
+    setForgotPasswordDialogIsOpen(true);
+  }
+
+  function handleForgotPasswordDialogClose() {
+    setForgotPasswordDialogIsOpen(false);
+  }
+
+  function handlePasswordResetRequest() {
+    handleInfoSnackbarOpen();
+    setForgotPasswordEmail("");
+    handleForgotPasswordDialogClose();
+  }
 
   function toggleAccount(event) {
     event.preventDefault();
@@ -67,156 +110,229 @@ function Auth(props) {
     }
   }
   return (
-    <div className="auth-main" style={{ backgroundImage: `url(${Clouds})` }}>
-      <Typography variant="h2" align="center" className="auth-title">
-        The Android<span style={{ fontFamily: "sans-serif" }}>'</span>s Dungeon
-      </Typography>
-      <Typography variant="h4" align="center" className="auth-subtitle">
-        A Simpsons Fan Page
-      </Typography>
-      <div className={`${account ? "login-card" : "login-card-closed"}`}>
-        <Card variant="outlined" className="auth-card">
-          <Typography variant="h4">Login</Typography>
-          <form onSubmit={handleLogin} className="login-form">
-            <FormControl>
-              <InputLabel htmlFor="username">Username or Email</InputLabel>
-              <Input
-                required
-                value={username}
-                id="username"
-                placeholder="Enter username or email"
-                startAdornment={
-                  <InputAdornment position="start">
-                    <AccountCircle />
-                  </InputAdornment>
-                }
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </FormControl>
-            <FormControl>
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Input
-                required
-                value={password}
-                placeholder="Enter password"
-                id="password"
-                startAdornment={
-                  <InputAdornment position="start">
-                    <LockIcon />
-                  </InputAdornment>
-                }
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-              />
-            </FormControl>
-            <Button variant="contained" color="primary" type="submit">
-              Submit
-            </Button>
-          </form>
-          <Typography variant="h6">
-            No account?{" "}
-            <Link href="#" onClick={toggleAccount}>
-              Register Here
-            </Link>
-          </Typography>
-        </Card>
+    <>
+      <div className="auth-main" style={{ backgroundImage: `url(${Clouds})` }}>
+        <Typography variant="h2" align="center" className="auth-title">
+          The Android<span style={{ fontFamily: "sans-serif" }}>'</span>s
+          Dungeon
+        </Typography>
+        <Typography variant="h4" align="center" className="auth-subtitle">
+          A Simpsons Fan Page
+        </Typography>
+        <div className={`${account ? "login-card" : "login-card-closed"}`}>
+          <Card variant="outlined" className="auth-card">
+            <Typography variant="h4">Login</Typography>
+            <form onSubmit={handleLogin} className="login-form">
+              <FormControl>
+                <InputLabel htmlFor="username">Username or Email</InputLabel>
+                <Input
+                  required
+                  value={username}
+                  id="username"
+                  placeholder="Enter username or email"
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <AccountCircle />
+                    </InputAdornment>
+                  }
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </FormControl>
+              <FormControl>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input
+                  required
+                  value={password}
+                  placeholder="Enter password"
+                  id="password"
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <LockIcon />
+                    </InputAdornment>
+                  }
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                />
+              </FormControl>
+              <Button variant="contained" color="primary" type="submit">
+                Submit
+              </Button>
+              <Button
+                type="button"
+                size="small"
+                color="secondary"
+                className="forgot-password-btn"
+                onClick={handleForgotPasswordDialogOpen}
+              >
+                Forgot Password?
+              </Button>
+            </form>
+            <Typography variant="h6">
+              No account?{" "}
+              <Link href="#" onClick={toggleAccount}>
+                Register Here
+              </Link>
+            </Typography>
+          </Card>
+        </div>
+        <div
+          className={`${account ? "register-card-closed" : "register-card"}`}
+        >
+          <Card variant="outlined" className="auth-card">
+            <Typography variant="h4">Register</Typography>
+            <form onSubmit={handleRegister} className="register-form">
+              <FormControl>
+                <InputLabel htmlFor="newUsername">Username</InputLabel>
+                <Input
+                  required
+                  value={newUsername}
+                  id="newUsername"
+                  placeholder="Enter username"
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <PersonIcon />
+                    </InputAdornment>
+                  }
+                  onChange={(e) => setNewUsername(e.target.value)}
+                />
+              </FormControl>
+              <FormControl>
+                <InputLabel htmlFor="email">Email</InputLabel>
+                <Input
+                  type="email"
+                  required
+                  value={email}
+                  id="email"
+                  placeholder="Enter email"
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <EmailIcon />
+                    </InputAdornment>
+                  }
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormControl>
+              <FormControl error={!passwordsMatch}>
+                <InputLabel htmlFor="newPassword">Password</InputLabel>
+                <Input
+                  required
+                  value={newPassword}
+                  aria-describedby="new-password-helper-text"
+                  id="newPassword"
+                  placeholder="Enter password"
+                  error={!passwordsMatch}
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <LockIcon />
+                    </InputAdornment>
+                  }
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  type="password"
+                />
+                {!passwordsMatch && (
+                  <FormHelperText id="new-password-helper-text">
+                    Passwords Do Not Match
+                  </FormHelperText>
+                )}
+              </FormControl>
+              <FormControl error={!passwordsMatch}>
+                <InputLabel htmlFor="confirmPassword">
+                  Confirm Password
+                </InputLabel>
+                <Input
+                  required
+                  value={confirmPassword}
+                  aria-describedby="confirm-password-helper-text"
+                  id="confirm"
+                  placeholder="Confirm password"
+                  error={!passwordsMatch}
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <VpnKeyIcon />
+                    </InputAdornment>
+                  }
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  type="password"
+                />
+                {!passwordsMatch && (
+                  <FormHelperText id="confirm-password-helper-text">
+                    Passwords Do Not Match
+                  </FormHelperText>
+                )}
+              </FormControl>
+              <Button variant="contained" color="primary" type="submit">
+                Submit
+              </Button>
+            </form>
+            <Typography variant="h6" className="register-text">
+              Already have an account?{" "}
+              <Link href="#" onClick={toggleAccount}>
+                Login Here
+              </Link>
+            </Typography>
+          </Card>
+        </div>
       </div>
-      <div className={`${account ? "register-card-closed" : "register-card"}`}>
-        <Card variant="outlined" className="auth-card">
-          <Typography variant="h4">Register</Typography>
-          <form onSubmit={handleRegister} className="register-form">
-            <FormControl>
-              <InputLabel htmlFor="newUsername">Username</InputLabel>
-              <Input
-                required
-                value={newUsername}
-                id="newUsername"
-                placeholder="Enter username"
-                startAdornment={
-                  <InputAdornment position="start">
-                    <PersonIcon />
-                  </InputAdornment>
-                }
-                onChange={(e) => setNewUsername(e.target.value)}
-              />
-            </FormControl>
-            <FormControl>
-              <InputLabel htmlFor="email">Email</InputLabel>
-              <Input
-                type="email"
-                required
-                value={email}
-                id="email"
-                placeholder="Enter email"
-                startAdornment={
-                  <InputAdornment position="start">
-                    <EmailIcon />
-                  </InputAdornment>
-                }
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </FormControl>
-            <FormControl error={!passwordsMatch}>
-              <InputLabel htmlFor="newPassword">Password</InputLabel>
-              <Input
-                required
-                value={newPassword}
-                aria-describedby="new-password-helper-text"
-                id="newPassword"
-                placeholder="Enter password"
-                error={!passwordsMatch}
-                startAdornment={
-                  <InputAdornment position="start">
-                    <LockIcon />
-                  </InputAdornment>
-                }
-                onChange={(e) => setNewPassword(e.target.value)}
-                type="password"
-              />
-              {!passwordsMatch && (
-                <FormHelperText id="new-password-helper-text">
-                  Passwords Do Not Match
-                </FormHelperText>
-              )}
-            </FormControl>
-            <FormControl error={!passwordsMatch}>
-              <InputLabel htmlFor="confirmPassword">
-                Confirm Password
-              </InputLabel>
-              <Input
-                required
-                value={confirmPassword}
-                aria-describedby="confirm-password-helper-text"
-                id="confirm"
-                placeholder="Confirm password"
-                error={!passwordsMatch}
-                startAdornment={
-                  <InputAdornment position="start">
-                    <VpnKeyIcon />
-                  </InputAdornment>
-                }
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                type="password"
-              />
-              {!passwordsMatch && (
-                <FormHelperText id="confirm-password-helper-text">
-                  Passwords Do Not Match
-                </FormHelperText>
-              )}
-            </FormControl>
-            <Button variant="contained" color="primary" type="submit">
-              Submit
-            </Button>
-          </form>
-          <Typography variant="h6" className="register-text">
-            Already have an account?{" "}
-            <Link href="#" onClick={toggleAccount}>
-              Login Here
-            </Link>
-          </Typography>
-        </Card>
-      </div>
-    </div>
+      <Dialog
+        open={forgotPasswordDialogIsOpen}
+        onClose={handleForgotPasswordDialogClose}
+        disableBackdropClick={true}
+        aria-labelledby="forgot-password-dialog"
+      >
+        <DialogTitle id="forgot-password-dialog">Forgot Password?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Enter the email address associated with your account to receive a
+            password reset link
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Email Address"
+            type="email"
+            fullWidth
+            value={forgotPasswordEmail}
+            onChange={(e) => setForgotPasswordEmail(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleForgotPasswordDialogClose} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handlePasswordResetRequest} color="primary">
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={infoSnackbarIsOpen}
+        autoHideDuration={4000}
+        onClose={handleInfoSnackbarClose}
+      >
+        <Alert
+          onClose={handleInfoSnackbarClose}
+          variant="filled"
+          severity="info"
+        >
+          Password reset link sent. Please check your email.
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={errorSnackbarIsOpen}
+        autoHideDuration={4000}
+        onClose={handleErrorSnackbarClose}
+      >
+        <Alert
+          onClose={handleErrorSnackbarClose}
+          variant="filled"
+          severity="error"
+        >
+          No user with that email exists. Please try again.
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
 
