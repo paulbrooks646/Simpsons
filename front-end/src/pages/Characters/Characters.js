314b619755f94c839a451e0ea88e9d7e04719284
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-17 09:30:32 -0600

    characters skeleton structure

---
+++ front-end/src/pages/Characters/Characters.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import "./Characters.scss"
+
+export default function Characters(props) {
+
+    return (
+        <div>Characters</div>
+    )
+}
--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -6,6 +6,7 @@ import Episodes from "./pages/Episodes/Episodes";
 import Profile from "./pages/Profile/Profile";
 import SingleEpisode from "./pages/SingleEpisode/SingleEpisode";
 import Error from "./pages/Error/Error";
+import Characters from "./pages/Characters/Characters";
 
 export default (
   <Switch>
@@ -14,6 +15,7 @@ export default (
     <Route path="/profile/:user_id" component={Profile} />
     <Route exact path="/episodes" component={Episodes} />
     <Route path="/episodes/:episode" component={SingleEpisode} />
+    <Route path="/characters" component={Characters}/>
     <Route path="*" component={Error} />
   </Switch>
 );
