ac0df1d89df353011cfdfaee043975550cf790e1
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-19 11:46:02 -0600

    dashboard

--- front-end/src/pages/Dashboard/Dashboard.js
+++ front-end/src/pages/Dashboard/Dashboard.js
@@ -12,6 +12,7 @@ export default function Dashboard(props) {
         style={{ backgroundImage: `url(${Background})` }}
       >
         <h1 className="dashboard-title">Welcome to The Android's Dungeon</h1>
+        <h2>This is by far the greatest website of all time!</h2>
       </div>
     </Page>
   );
