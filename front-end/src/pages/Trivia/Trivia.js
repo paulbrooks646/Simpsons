bfd8ff134098971d86d9150b6e670d18956c7099
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-19 12:27:14 -0600

    trivia image

--- front-end/src/pages/Trivia/Trivia.js
+++ front-end/src/pages/Trivia/Trivia.js
@@ -1,65 +1,75 @@
-import React from "react"
-import Page from "../../components/Page"
-import Card from "@material-ui/core/Card"
-import Button from "@material-ui/core/Button"
+import React from "react";
+import Page from "../../components/Page";
+import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
 
 export default function Trivia(props) {
+  const yes = () => alert("A paddy wagon is now speeding to your home.");
 
-    const yes = () => alert("A paddy wagon is now speeding to your home.");
-    
-    const no = () =>
-      alert(
-        "You have chosen 'No', meaning that you've committed a crime, but don't want to confess. A paddy wagon is now speeding to your home."
-      );
+  const no = () =>
+    alert(
+      "You have chosen 'No', meaning that you've committed a crime, but don't want to confess. A paddy wagon is now speeding to your home."
+    );
 
-    return (
-      <Page>
-        <div
+  return (
+    <Page>
+      <div
+        style={{
+          height: "calc(100vh - 75px)",
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          flexDirection: "column",
+          color: "red",
+        }}
+      >
+        <h1>Trivia</h1>
+        <Card
           style={{
-            height: "calc(100vh - 75px)",
-            width: "100vw",
+            width: "500px",
+            height: "300px",
             display: "flex",
             justifyContent: "center",
-            alignItems: "center",
-                    textAlign: "center",
-                    flexDirection: 'column',
-            color: 'red'
+            flexDirection: "column",
           }}
-            >
-                <h1>Trivia</h1>
-          <Card
+        >
+          <div
             style={{
-              width: "500px",
-              height: "300px",
+              height: "100%",
               display: "flex",
-              justifyContent: "center",
               flexDirection: "column",
+              justifyContent: "space-evenly",
+              alignItems: "center",
             }}
           >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/7/7a/Chief_Wiggum.png"
+              alt="chief wiggums"
+              style={{ height: "100px" }}
+            />
+            <h2 style={{ color: "blue" }}>
+              If you've committed a crime and want to confess, click 'Yes'.
+              Otherwise, click 'No'
+            </h2>
             <div
               style={{
-                height: "100%",
                 display: "flex",
-                flexDirection: "column",
                 justifyContent: "space-evenly",
-                alignItems: "center",
+                width: "100%",
               }}
             >
-              <h2 style={{color: 'blue'}}>
-                If you've committed a crime and want to confess, click 'Yes'.
-                Otherwise, click 'No'
-              </h2>
-              <div style={{ display: "flex", justifyContent: "space-evenly", width: "100%" }}>
-                <Button variant="outlined" color="secondary" onClick={yes}>
-                  Yes
-                </Button>
-                <Button variant="outlined" color="secondary" onClick={no}>
-                  No
-                </Button>
-              </div>
+              <Button variant="outlined" color="secondary" onClick={yes}>
+                Yes
+              </Button>
+              <Button variant="outlined" color="secondary" onClick={no}>
+                No
+              </Button>
             </div>
-          </Card>
-        </div>
-      </Page>
-    );
+          </div>
+        </Card>
+      </div>
+    </Page>
+  );
 }
