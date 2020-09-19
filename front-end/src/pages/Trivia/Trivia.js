65461bd69df99e51e72b8e4f67f473072250b6cc
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-19 11:55:46 -0600

    trivia skeleton structure

---
+++ front-end/src/pages/Trivia/Trivia.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Page from "../../components/Page"
+
+export default function Trivia(props) {
+
+    return (
+      <Page>
+        <div>Trivia</div>
+      </Page>
+    );
+}
