cc3a1c6629ab664ad32dff214afa58123f7579b0
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-19 12:13:15 -0600

    trivia buttons

--- front-end/src/pages/Trivia/Trivia.js
+++ front-end/src/pages/Trivia/Trivia.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Page from "../../components/Page"
 import Card from "@material-ui/core/Card"
+import Button from "@material-ui/core/Button"
 
 export default function Trivia(props) {
 
@@ -16,15 +17,35 @@ export default function Trivia(props) {
             textAlign: "center",
           }}
         >
-          <Card style={{ width: "500px", height: "300px", display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
-            <div style={{height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center'}}>
+          <Card
+            style={{
+              width: "500px",
+              height: "300px",
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <div
+              style={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-evenly",
+                alignItems: "center",
+              }}
+            >
               <h2>
                 If you've committed a crime and want to confess, click 'Yes'.
                 Otherwise, click 'No'
               </h2>
-              <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
-                <button>Yes</button>
-                <button>No</button>
+              <div style={{ display: "flex", justifyContent: "space-evenly", width: "100%" }}>
+                <Button variant="outlined" color="secondary">
+                  Yes
+                </Button>
+                <Button variant="outlined" color="secondary">
+                  No
+                </Button>
               </div>
             </div>
           </Card>
