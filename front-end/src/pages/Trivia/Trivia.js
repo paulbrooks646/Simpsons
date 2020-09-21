c9997946cdc97280498e02db723da31a4efbfcae
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-21 16:00:51 -0600

    question 1

--- front-end/src/pages/Trivia/Trivia.js
+++ front-end/src/pages/Trivia/Trivia.js
@@ -1,134 +1,446 @@
-import React from "react";
+import React, { useState } from "react";
 import Page from "../../components/Page";
 import Card from "@material-ui/core/Card";
 import Button from "@material-ui/core/Button";
 import "./Trivia.scss";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
 
 export default function Trivia(props) {
+  const [maggie, setMaggie] = useState();
+
   return (
     <Page>
       <div className="trivia-main">
         <h1>Trivia</h1>
         <Card className="trivia-card">
           <div className="trivia-card-div">
-            <img src="" alt="" className="trivia-card-image" />
-            <h2 className="trivia-question"></h2>
-            <div className="trivia-answers">
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-            </div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/9/9d/Maggie_Simpson.png"
+              alt="Maggie Simpson"
+              className="trivia-card-image"
+            />
+            <h2 className="trivia-question">
+              When Maggie is scanned in the supermarket how much does she cost?
+            </h2>
+
+            <FormControl component="fieldset" className="trivia-answers">
+              <RadioGroup
+                className="trivia-radio-group"
+                row
+                aria-label="maggie"
+                name="maggie"
+                value={maggie}
+                onChange={(e) => setMaggie(e.target.value)}
+              >
+                <FormControlLabel
+                  value="$1"
+                  control={<Radio style={{ color: "red" }} />}
+                  label="$1"
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value="$2"
+                  control={<Radio style={{ color: "red" }} />}
+                  label="$2"
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value="$3"
+                  control={<Radio style={{ color: "red" }} />}
+                  label="$3"
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value="$847.63"
+                  control={<Radio style={{ color: "red" }} />}
+                  label="$847.63"
+                  style={{ color: "red" }}
+                />
+              </RadioGroup>
+            </FormControl>
           </div>
         </Card>
         <Card className="trivia-card">
           <div className="trivia-card-div">
             <img src="" alt="" className="trivia-card-image" />
             <h2 className="trivia-question"></h2>
-            <div className="trivia-answers">
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-            </div>
+
+            <FormControl component="fieldset" className="trivia-answers">
+              <RadioGroup
+                className="trivia-radio-group"
+                row
+                aria-label=""
+                name=""
+                value={}
+                onChange={(e) => set(e.target.value)}
+              >
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+              </RadioGroup>
+            </FormControl>
           </div>
         </Card>
+        {/* <Card className="trivia-card">
+          <div className="trivia-card-div">
+            <img src="" alt="" className="trivia-card-image" />
+            <h2 className="trivia-question"></h2>
+
+            <FormControl component="fieldset" className="trivia-answers">
+              <RadioGroup
+                className="trivia-radio-group"
+                row
+                aria-label=""
+                name=""
+                value={}
+                onChange={(e) => set(e.target.value)}
+              >
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+              </RadioGroup>
+            </FormControl>
+          </div>
+        </Card>{" "}
         <Card className="trivia-card">
           <div className="trivia-card-div">
             <img src="" alt="" className="trivia-card-image" />
             <h2 className="trivia-question"></h2>
-            <div className="trivia-answers">
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-            </div>
+
+            <FormControl component="fieldset" className="trivia-answers">
+              <RadioGroup
+                className="trivia-radio-group"
+                row
+                aria-label=""
+                name=""
+                value={}
+                onChange={(e) => set(e.target.value)}
+              >
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+              </RadioGroup>
+            </FormControl>
           </div>
-        </Card>
+        </Card>{" "}
         <Card className="trivia-card">
           <div className="trivia-card-div">
             <img src="" alt="" className="trivia-card-image" />
             <h2 className="trivia-question"></h2>
-            <div className="trivia-answers">
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-            </div>
+
+            <FormControl component="fieldset" className="trivia-answers">
+              <RadioGroup
+                className="trivia-radio-group"
+                row
+                aria-label=""
+                name=""
+                value={}
+                onChange={(e) => set(e.target.value)}
+              >
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+              </RadioGroup>
+            </FormControl>
           </div>
-        </Card>
+        </Card>{" "}
         <Card className="trivia-card">
           <div className="trivia-card-div">
             <img src="" alt="" className="trivia-card-image" />
             <h2 className="trivia-question"></h2>
-            <div className="trivia-answers">
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-            </div>
+
+            <FormControl component="fieldset" className="trivia-answers">
+              <RadioGroup
+                className="trivia-radio-group"
+                row
+                aria-label=""
+                name=""
+                value={}
+                onChange={(e) => set(e.target.value)}
+              >
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+              </RadioGroup>
+            </FormControl>
           </div>
-        </Card>
+        </Card>{" "}
         <Card className="trivia-card">
           <div className="trivia-card-div">
             <img src="" alt="" className="trivia-card-image" />
             <h2 className="trivia-question"></h2>
-            <div className="trivia-answers">
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-            </div>
+
+            <FormControl component="fieldset" className="trivia-answers">
+              <RadioGroup
+                className="trivia-radio-group"
+                row
+                aria-label=""
+                name=""
+                value={}
+                onChange={(e) => set(e.target.value)}
+              >
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+              </RadioGroup>
+            </FormControl>
           </div>
-        </Card>
+        </Card>{" "}
         <Card className="trivia-card">
           <div className="trivia-card-div">
             <img src="" alt="" className="trivia-card-image" />
             <h2 className="trivia-question"></h2>
-            <div className="trivia-answers">
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-            </div>
+
+            <FormControl component="fieldset" className="trivia-answers">
+              <RadioGroup
+                className="trivia-radio-group"
+                row
+                aria-label=""
+                name=""
+                value={}
+                onChange={(e) => set(e.target.value)}
+              >
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+              </RadioGroup>
+            </FormControl>
           </div>
-        </Card>
+        </Card>{" "}
         <Card className="trivia-card">
           <div className="trivia-card-div">
             <img src="" alt="" className="trivia-card-image" />
             <h2 className="trivia-question"></h2>
-            <div className="trivia-answers">
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-            </div>
+
+            <FormControl component="fieldset" className="trivia-answers">
+              <RadioGroup
+                className="trivia-radio-group"
+                row
+                aria-label=""
+                name=""
+                value={}
+                onChange={(e) => set(e.target.value)}
+              >
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+              </RadioGroup>
+            </FormControl>
           </div>
-        </Card>
+        </Card>{" "}
         <Card className="trivia-card">
           <div className="trivia-card-div">
             <img src="" alt="" className="trivia-card-image" />
             <h2 className="trivia-question"></h2>
-            <div className="trivia-answers">
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-            </div>
+
+            <FormControl component="fieldset" className="trivia-answers">
+              <RadioGroup
+                className="trivia-radio-group"
+                row
+                aria-label=""
+                name=""
+                value={}
+                onChange={(e) => set(e.target.value)}
+              >
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+                <FormControlLabel
+                  value=""
+                  control={<Radio style={{ color: "red" }} />}
+                  label=""
+                  style={{ color: "red" }}
+                />
+              </RadioGroup>
+            </FormControl>
           </div>
-        </Card>
-        <Card className="trivia-card">
-          <div className="trivia-card-div">
-            <img src="" alt="" className="trivia-card-image" />
-            <h2 className="trivia-question"></h2>
-            <div className="trivia-answers">
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-              <Button variant="outlined" color="secondary"></Button>
-            </div>
-          </div>
-        </Card>
+        </Card> */}
       </div>
     </Page>
   );
