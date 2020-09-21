2fcc1cabd9e62c09ae1c61c64b57228c758489df
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-21 17:02:06 -0600

    minor tweaks

--- front-end/src/pages/Dashboard/Dashboard.js
+++ front-end/src/pages/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import Page from "../../components/Page";
 import "./Dashboard.scss";
 import Background from "../../images/androids-dungeon-bg.png";
+import Card from "@material-ui/core/Card"
 
 
 export default function Dashboard(props) {
@@ -11,8 +12,8 @@ export default function Dashboard(props) {
         className="dashboard-main"
         style={{ backgroundImage: `url(${Background})` }}
       >
-        <h1 className="dashboard-title">Welcome to The Android's Dungeon</h1>
-        <h2>This is by far the greatest website of all time!</h2>
+        {/* <h1 className="dashboard-title">Welcome to The Android's Dungeon</h1> */}
+        <Card className="dashboard-card"><h2>Man Getting Hit By Football!</h2></Card>
       </div>
     </Page>
   );
