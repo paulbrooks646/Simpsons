c549f686e5967f973d61136ec52a2486f773c311
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-21 15:03:09 -0600

    trivia layout

--- front-end/src/pages/Trivia/Trivia.js
+++ front-end/src/pages/Trivia/Trivia.js
@@ -5,35 +5,127 @@ import Button from "@material-ui/core/Button";
 import "./Trivia.scss";
 
 export default function Trivia(props) {
-  const yes = () => alert("A paddy wagon is now speeding to your home.");
-
-  const no = () =>
-    alert(
-      "You have chosen 'No', meaning that you've committed a crime, but don't want to confess. A paddy wagon is now speeding to your home."
-    );
-
   return (
     <Page>
       <div className="trivia-main">
         <h1>Trivia</h1>
         <Card className="trivia-card">
-          <div classsName="trivia-card-div">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/en/7/7a/Chief_Wiggum.png"
-              alt="chief wiggums"
-              className="trivia-card-image"
-            />
-            <h2 className="trivia-question">
-              If you've committed a crime and want to confess, click 'Yes'.
-              Otherwise, click 'No'
-            </h2>
+          <div className="trivia-card-div">
+            <img src="" alt="" className="trivia-card-image" />
+            <h2 className="trivia-question"></h2>
             <div className="trivia-answers">
-              <Button variant="outlined" color="secondary" onClick={yes}>
-                Yes
-              </Button>
-              <Button variant="outlined" color="secondary" onClick={no}>
-                No
-              </Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+            </div>
+          </div>
+        </Card>
+        <Card className="trivia-card">
+          <div className="trivia-card-div">
+            <img src="" alt="" className="trivia-card-image" />
+            <h2 className="trivia-question"></h2>
+            <div className="trivia-answers">
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+            </div>
+          </div>
+        </Card>
+        <Card className="trivia-card">
+          <div className="trivia-card-div">
+            <img src="" alt="" className="trivia-card-image" />
+            <h2 className="trivia-question"></h2>
+            <div className="trivia-answers">
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+            </div>
+          </div>
+        </Card>
+        <Card className="trivia-card">
+          <div className="trivia-card-div">
+            <img src="" alt="" className="trivia-card-image" />
+            <h2 className="trivia-question"></h2>
+            <div className="trivia-answers">
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+            </div>
+          </div>
+        </Card>
+        <Card className="trivia-card">
+          <div className="trivia-card-div">
+            <img src="" alt="" className="trivia-card-image" />
+            <h2 className="trivia-question"></h2>
+            <div className="trivia-answers">
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+            </div>
+          </div>
+        </Card>
+        <Card className="trivia-card">
+          <div className="trivia-card-div">
+            <img src="" alt="" className="trivia-card-image" />
+            <h2 className="trivia-question"></h2>
+            <div className="trivia-answers">
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+            </div>
+          </div>
+        </Card>
+        <Card className="trivia-card">
+          <div className="trivia-card-div">
+            <img src="" alt="" className="trivia-card-image" />
+            <h2 className="trivia-question"></h2>
+            <div className="trivia-answers">
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+            </div>
+          </div>
+        </Card>
+        <Card className="trivia-card">
+          <div className="trivia-card-div">
+            <img src="" alt="" className="trivia-card-image" />
+            <h2 className="trivia-question"></h2>
+            <div className="trivia-answers">
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+            </div>
+          </div>
+        </Card>
+        <Card className="trivia-card">
+          <div className="trivia-card-div">
+            <img src="" alt="" className="trivia-card-image" />
+            <h2 className="trivia-question"></h2>
+            <div className="trivia-answers">
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+            </div>
+          </div>
+        </Card>
+        <Card className="trivia-card">
+          <div className="trivia-card-div">
+            <img src="" alt="" className="trivia-card-image" />
+            <h2 className="trivia-question"></h2>
+            <div className="trivia-answers">
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
+              <Button variant="outlined" color="secondary"></Button>
             </div>
           </div>
         </Card>
