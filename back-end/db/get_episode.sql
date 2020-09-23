2a48cf4d11815747acd24a4fd5786e9d1c578bdd
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-23 12:04:27 -0600

    get episode

--- back-end/db/get_episode.sql
+++ back-end/db/get_episode.sql
@@ -1,4 +1,4 @@
-select episodes.*, ratings_reviews.*
+select episodes.*, ratings_reviews.ratings_reviews_id, ratings_reviews.user_id, ratings_reviews.rating, ratings_reviews.review, ratings_reviews.username, ratings_reviews.profile_pic 
 from episodes
-inner join ratings_reviews on episodes.episode_name=ratings_reviews.episode_name
+left outer join ratings_reviews on episodes.episode_name=ratings_reviews.episode_name
 where episodes.episode_name = $1
