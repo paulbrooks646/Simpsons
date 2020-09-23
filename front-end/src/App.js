7408506f121d0d174dcea54e86caa9f3d3d218d5
Philip Ng <philipn@corebridge.net>
2020-09-23 12:28:30 -0600

    override material-ui primary and secondary colors

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import Routes from "./routes";
 
-function App() {
-  return <>{Routes}</>;
-}
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#45aaf2",
+      dark: " #4b7bec",
+    },
+    secondary: {
+      main: "#fc5c65",
+      dark: "#eb3b5a",
+    },
+  },
+});
+
+const App = () => <MuiThemeProvider theme={theme}>{Routes}</MuiThemeProvider>;
 
 export default App;
