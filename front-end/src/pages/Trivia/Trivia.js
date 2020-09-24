f66dd0bea18a67e22d191187037807fd0b6763bb
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-24 15:10:10 -0600

    trivia links

--- front-end/src/pages/Trivia/Trivia.js
+++ front-end/src/pages/Trivia/Trivia.js
@@ -1,27 +1,40 @@
-import React from "react"
-import "./Trivia.scss"
-import Page from "../../components/Page"
-import Card from "@material-ui/core/Card"
+import React from "react";
+import "./Trivia.scss";
+import Page from "../../components/Page";
+import Card from "@material-ui/core/Card";
+import { Link } from "react-router-dom";
 
 export default function trivia() {
-
-    return (
-      <Page>
-        <div className="trivia-main">
-          {/* <h1 className="trivia-title">Trivia</h1> */}
-          <h2 className="trivia-subtitle">Choose a quiz!</h2>
-          <div className="trivia-card-div">
+  return (
+    <Page>
+      <div className="trivia-main">
+        <h2 className="trivia-subtitle">Choose a quiz!</h2>
+        <div className="trivia-card-div">
+          <Link to="/trivia/1">
             <Card className="trivia-card">Beginner</Card>
+          </Link>
+          <Link to="/trivia/2">
             <Card className="trivia-card">Advanced</Card>
+          </Link>
+          <Link to="/trivia/3">
             <Card className="trivia-card">Expert</Card>
+          </Link>
+          <Link to="/trivia/4">
             <Card className="trivia-card">Name that Character</Card>
+          </Link>
+          <Link to="/trivia/5">
             <Card className="trivia-card">Finish the Quote</Card>
+          </Link>
+          <Link to="/trivia/6">
             <Card className="trivia-card">Who Said That</Card>
+          </Link>
+          <Link to="/trivia/7">
             <Card className="trivia-card">
               Which Simpsons Charater are You?
             </Card>
-          </div>
+          </Link>
         </div>
-      </Page>
-    );
+      </div>
+    </Page>
+  );
 }
