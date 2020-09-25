a45b036edf7f9539d4430e8f7540e0e5455f1c8e
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-25 13:59:37 -0600

    dashboard positioning

--- front-end/src/pages/Dashboard/Dashboard.js
+++ front-end/src/pages/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Dashboard.scss";
 import Background from "../../images/androids-dungeon-bg.png";
 import Card from "@material-ui/core/Card";
+import { Link } from "react-router-dom";
 
 export default function Dashboard(props) {
   return (
@@ -16,24 +17,68 @@ export default function Dashboard(props) {
             id="dashboard-card-top"
             className="dashboard-main"
             style={{ backgroundImage: `url(${Background})` }}
-          ><h1>Profile</h1></Card>
+          >
+            <h1
+              style={{
+                border: "solid",
+                width: "180px",
+                position: "relative",
+                top: "25px",
+              }}
+            >
+              Profile
+            </h1>
+          </Card>
           <Card
             id="dashboard-card-top"
             className="dashboard-main"
             style={{ backgroundImage: `url(${Background})` }}
-          ><h1>Episodes</h1></Card>
+          >
+            <h1
+              style={{
+                border: "solid",
+                width: "180px",
+                position: "relative",
+                top: "25px",
+              }}
+            >
+              Episodes
+            </h1>
+          </Card>
         </div>
         <div className="bottom-div-cards">
           <Card
             id="dashboard-card-bottom"
             className="dashboard-main"
             style={{ backgroundImage: `url(${Background})` }}
-          ><h1>Characters</h1></Card>
+          >
+            <h1
+              style={{
+                border: "solid",
+                width: "180px",
+                position: "relative",
+                top: "60px",
+              }}
+            >
+              Characters
+            </h1>
+          </Card>
           <Card
             id="dashboard-card-bottom"
             className="dashboard-main"
             style={{ backgroundImage: `url(${Background})` }}
-          ><h1>Trivia</h1></Card>
+          >
+            <h1
+              style={{
+                border: "solid",
+                width: "180px",
+                position: "relative",
+                top: "60px",
+              }}
+            >
+              Trivia
+            </h1>
+          </Card>
         </div>
       </div>
     </div>
