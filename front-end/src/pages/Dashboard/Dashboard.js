cdaa89dbd7e8594c5ce0255bc56d058e3c674196
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-25 12:51:31 -0600

    dashboard cards

--- front-end/src/pages/Dashboard/Dashboard.js
+++ front-end/src/pages/Dashboard/Dashboard.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./Dashboard.scss";
 import Background from "../../images/androids-dungeon-bg.png";
-// import Card from "@material-ui/core/Card"
-
+import Card from "@material-ui/core/Card";
 
 export default function Dashboard(props) {
   return (
-    
-      <div
-        className="dashboard-main"
-        style={{ backgroundImage: `url(${Background})` }}
-      >
-        {/* <h1 className="dashboard-title">Welcome to The Android's Dungeon</h1> */}
-
-      </div>
-  
+    <div
+      className="dashboard-main"
+      style={{ backgroundImage: `url(${Background})` }}
+    >
+      <h1 className="dashboard-title">The Android's Dungeon</h1>
+      <Card className="dashboard-card"></Card>
+      <Card className="dashboard-card"></Card>
+      <Card className="dashboard-card"></Card>
+      <Card className="dashboard-card"></Card>
+    </div>
   );
 }
