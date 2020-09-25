9638ffafc9a348191edf2b42d8651f6cc2e8d950
Philip Ng <philipn@corebridge.net>
2020-09-25 14:19:11 -0600

    modified trivia page

--- front-end/src/pages/Trivia/Trivia.js
+++ front-end/src/pages/Trivia/Trivia.js
@@ -1,39 +1,87 @@
 import React from "react";
-import "./Trivia.scss";
-import Page from "../../components/Page";
-import Card from "@material-ui/core/Card";
 import { Link } from "react-router-dom";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardMedia from "@material-ui/core/CardMedia";
+import Carousel from "react-material-ui-carousel";
+import Page from "../../components/Page";
+import SimpsonsCharacters from "../../images/TriviaPage/simpsons-characters.jpg";
+import WhoSaidThat from "../../images/TriviaPage/who-said-that.jpg";
+import ProfessorFrinkChalkboard from "../../images/TriviaPage/professor-frink-chalkboard.gif";
+import BartSimpsonTakingTest from "../../images/TriviaPage/bart-simpson-taking-test.jpeg";
+import "./Trivia.scss";
 
-export default function trivia() {
+export default function Trivia() {
+  const items = [
+    {
+      triviaName: "Beginner",
+      triviaURL: "/trivia/1",
+      triviaImage: "https://via.placeholder.com/500",
+    },
+    {
+      triviaName: "Advanced",
+      triviaURL: "/trivia/2",
+      triviaImage: BartSimpsonTakingTest,
+    },
+    {
+      triviaName: "Expert",
+      triviaURL: "/trivia/3",
+      triviaImage: ProfessorFrinkChalkboard,
+    },
+    {
+      triviaName: "Name That Character",
+      triviaURL: "/trivia/4",
+      triviaImage: "https://via.placeholder.com/500",
+    },
+    {
+      triviaName: "Finish The Quote",
+      triviaURL: "/trivia/5",
+      triviaImage: "https://via.placeholder.com/500",
+    },
+    {
+      triviaName: "Who Said That",
+      triviaURL: "/trivia/6",
+      triviaImage: WhoSaidThat,
+    },
+    {
+      triviaName: "Which Simpsons Character Are You?",
+      triviaURL: "/personality-test",
+      triviaImage: SimpsonsCharacters,
+    },
+  ];
+
   return (
     <Page>
       <div className="trivia-main">
-        <h2 className="trivia-subtitle">Choose a quiz!</h2>
-        <div className="trivia-card-div">
-          <Link to="/trivia/1">
-            <Card className="trivia-card">Beginner</Card>
-          </Link>
-          <Link to="/trivia/2">
-            <Card className="trivia-card">Advanced</Card>
-          </Link>
-          <Link to="/trivia/3">
-            <Card className="trivia-card">Expert</Card>
-          </Link>
-          <Link to="/trivia/4">
-            <Card className="trivia-card">Name that Character</Card>
-          </Link>
-          <Link to="/trivia/5">
-            <Card className="trivia-card">Finish the Quote</Card>
-          </Link>
-          <Link to="/trivia/6">
-            <Card className="trivia-card">Who Said That</Card>
-          </Link>
-          <Link to="/personality-test">
-            <Card className="trivia-card">
-              Which Simpsons Charater are You?
+        <h2>Choose a quiz!</h2>
+        <Carousel
+          autoPlay={false}
+          navButtonsAlwaysVisible={true}
+          navButtonsAlwaysInvisible={false}
+          fullHeightHover={false}
+          className="trivia-carousel"
+        >
+          {items.map((item, index) => (
+            <Card key={index}>
+              <Link
+                to={item.triviaURL}
+                className="trivia-carousel-link-wrapper"
+              >
+                <CardActionArea>
+                  <CardMedia
+                    className="trivia-carousel-image"
+                    image={item.triviaImage}
+                  />
+                  <CardContent>
+                    <Typography variant="h4">{item.triviaName}</Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Link>
             </Card>
-          </Link>
-        </div>
+          ))}
+        </Carousel>
       </div>
     </Page>
   );
