cb9dd09f1e4eab0e978ad72c804fa70a95732cb6
paulbrooks646 <paulbrooks646@gmail.com>
2020-09-25 13:43:12 -0600

    dashboard

--- front-end/src/pages/Dashboard/Dashboard.js
+++ front-end/src/pages/Dashboard/Dashboard.js
@@ -13,27 +13,27 @@ export default function Dashboard(props) {
       <div className="dashboard-card-div">
         <div className="top-div-cards">
           <Card
-            id="dashboard-card"
+            id="dashboard-card-top"
             className="dashboard-main"
             style={{ backgroundImage: `url(${Background})` }}
-          ></Card>
+          ><h1>Profile</h1></Card>
           <Card
-            id="dashboard-card"
+            id="dashboard-card-top"
             className="dashboard-main"
             style={{ backgroundImage: `url(${Background})` }}
-          ></Card>
+          ><h1>Episodes</h1></Card>
         </div>
         <div className="bottom-div-cards">
           <Card
-            id="dashboard-card"
+            id="dashboard-card-bottom"
             className="dashboard-main"
             style={{ backgroundImage: `url(${Background})` }}
-          ></Card>
+          ><h1>Characters</h1></Card>
           <Card
-            id="dashboard-card"
+            id="dashboard-card-bottom"
             className="dashboard-main"
             style={{ backgroundImage: `url(${Background})` }}
-          ></Card>
+          ><h1>Trivia</h1></Card>
         </div>
       </div>
     </div>
